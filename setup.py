d835537b8d05eb503995c5385a7d8861c6dae524
Nathan Davis Barrett <76917233+NathanDavisBarrett@users.noreply.github.com>
2021-01-16 10:49:11 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='NathanLibrary',
+      version='2021.1.1',
+      description='Library for solving chemical engineering problems',
+      author='Nathan Barrett',
+      author_email='ndb.nathan.barrett@gmail.com',
+      url='http://github.com/NathanDavisBarrett',
+     )
